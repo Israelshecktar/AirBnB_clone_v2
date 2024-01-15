6e23dbf685b5b7968010fd9ef2e46f41dd37d285
Israelshecktar <iamshecktar1996@gmail.com>
2024-01-15 11:38:54 +0100

    prepared mySQL server for this project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Create the 'hbnb_test_db' database if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the 'hbnb_test' user with the specified password if it does not already exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on the 'hbnb_test_db' database to the 'hbnb_test' user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on the 'performance_schema' database to the 'hbnb_test' user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Apply the changes made by the GRANT statements
+FLUSH PRIVILEGES;
