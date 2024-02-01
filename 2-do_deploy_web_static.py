6da3b90fc942891e88312f9cf699af2431f5effd
Israelshecktar <iamshecktar1996@gmail.com>
2024-02-01 14:13:56 +0100

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,6 +5,7 @@ A fabric script that pack and distributes an archive on server
 
 
 from fabric.api import *
+from fabric.contrib import files
 import os
 from datetime import datetime
 
@@ -31,18 +32,17 @@ def do_deploy(archive_path):
     """
     Distribute archive.
     """
-    if os.path.exists(archive_path):
+    if files.exists(archive_path):
         archived_file = archive_path[9:]
         newest_version = "/data/web_static/releases/" + archived_file[:-4]
         archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
+        put(archive_path, "/tmp/", use_sudo=True)
         run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
+        run("sudo tar -xzf {0} -C {1}/".format(archived_file,
                                              newest_version))
         run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo mv {0}/web_static/* {0}/".format(newest_version))
+        run("sudo rm -rf {0}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
